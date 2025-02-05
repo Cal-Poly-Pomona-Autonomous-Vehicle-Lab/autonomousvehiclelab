5431dcb37054daabb4c2eb449f7a1bf067f2a910
Conspiciens <fernandezjohann08@gmail.com>
2025-02-05 11:50:01 -0800

    Handle when the image is null

--- isaac_ros-dev/src/data_logger/src/data_logger_node.cpp
+++ isaac_ros-dev/src/data_logger/src/data_logger_node.cpp
@@ -113,6 +113,15 @@ class DataLogger: public rclcpp::Node {
 
 
 	private:
+		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr m_subscription_steering_angle;  
+		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_front_camera; 
+		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_left_camera; 
+		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_right_camera;
+
+		sensor_msgs::msg::Image::ConstSharedPtr m_left_cam; 
+		sensor_msgs::msg::Image::ConstSharedPtr m_right_cam; 
+		sensor_msgs::msg::Image::ConstSharedPtr m_front_cam;
+
 		std::ofstream m_current_file; 
 
 		/* '/logging' should contain logging folder */
@@ -148,14 +157,16 @@ class DataLogger: public rclcpp::Node {
 			double steering_angle = msg->angular.z;
             const int steering_angle_int = steering_angle * 61.0 + 512.0;
 
-			/* cv::Mat left_frame_cam; 
-			cv::Mat right_frame_cam;
-			cv::Mat front_frame_cam; */ 
+			if (m_front_cam == NULL)
+				return; 
+			if (m_left_cam == NULL)
+				return; 
+			if (m_right_cam == NULL)
+				return;
 
 			cv_bridge::CvImagePtr cv_front_ptr; 
             cv_bridge::CvImagePtr cv_left_ptr; 
             cv_bridge::CvImagePtr cv_right_ptr; 
-            
 
 			cv_front_ptr = cv_bridge::toCvCopy(m_front_cam, sensor_msgs::image_encodings::BGR8);
             cv_left_ptr = cv_bridge::toCvCopy(m_left_cam, sensor_msgs::image_encodings::BGR8);
@@ -205,16 +216,6 @@ class DataLogger: public rclcpp::Node {
 
 			RCLCPP_INFO(this->get_logger(), "%s", front_cam_result.c_str()); 
 		} 
-
-		rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr m_subscription_steering_angle;  
-		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_front_camera; 
-		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_left_camera; 
-		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr m_subscription_right_camera;
-
-		sensor_msgs::msg::Image::ConstSharedPtr m_left_cam; 
-		sensor_msgs::msg::Image::ConstSharedPtr m_right_cam; 
-		sensor_msgs::msg::Image::ConstSharedPtr m_front_cam;
-		 
 }; 
 
 int main(int argc, char *argv[]){
