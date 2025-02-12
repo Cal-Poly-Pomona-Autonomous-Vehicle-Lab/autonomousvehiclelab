59f74d673a037d7360dd7f22109cbcad7ff19510
Conspiciens <fernandezjohann08@gmail.com>
2025-02-11 23:13:27 -0800

    Change everything to use sim_time, attempt to unify for data logger 2

--- isaac_ros-dev/launch/bringup_logging_launch.py
+++ isaac_ros-dev/launch/bringup_logging_launch.py
@@ -3,39 +3,42 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
 import os
 
 # Launch file for starting all nodes
 # TODO: Investigate node compositors and more efficient ways of bringing the system up
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     data_logger_node = Node(
-        package='data_logger_py', 
-        namespace='data_logger_py', 
-        executable='data_logger_py_node'
+        package='data_logger', 
+        namespace='data_logger', 
+        executable='data_logger_node', 
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
 
     web_video_server_node = Node(
         package='web_video_server',
         namespace='web_video_server',
-        executable='web_video_server'
-        
+        executable='web_video_server',
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     motor_control_node = Node(
         package='motor_control_pkg',
         namespace='motor_control_pkg',
-        executable='motor_control_node'
-        
+        executable='motor_control_node',
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     image_stitcher_node = Node(
         package='image_stitcher',
         namespace='stiched_images',
-        executable='image_stitcher_node'
+        executable='image_stitcher_node',
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     
@@ -47,7 +50,10 @@ def generate_launch_description():
                     FindPackageShare('twist_mux'),
                     'launch/twist_mux_launch.py'
                 ])
-            ])
+            ]),
+            launch_arguments={
+                'use_sim_time': use_sim_time
+            }
         ),
         motor_control_node,
         image_stitcher_node,
